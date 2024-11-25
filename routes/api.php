b5b38f55b7825d87795d7dafcce5ce600d70cc27
Carolina Picheictt <picheictt.carolina@gmail.com>
2024-11-25 17:39:35 -0300

    Back para um select

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return response()->json(['message' => 'Oi do Laravel!']);
-});
+//Route::get('/hello', function () {
+  //  return response()->json(['message' => 'Oi do Laravel!']);
+//});
+
+Route::get('/teste', [App\Http\Controllers\TesteController::class, 'index'])->name('teste.index');
+
+
